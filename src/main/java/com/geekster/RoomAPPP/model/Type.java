dea5b342db7794af937d466cfd743d51c3262244
Mithilesh <ashish841407@gmail.com>
2023-10-27 16:15:26 +0530

    RoomApp

---
+++ src/main/java/com/geekster/RoomAPPP/model/Type.java
@@ -0,0 +1,7 @@
+package com.geekster.RoomAPPP.model;
+
+public enum Type {
+
+    AC,
+    NON_AC
+}
---
+++ src/test/java/com/geekster/RoomAPPP/RoomApppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.RoomAPPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomApppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
